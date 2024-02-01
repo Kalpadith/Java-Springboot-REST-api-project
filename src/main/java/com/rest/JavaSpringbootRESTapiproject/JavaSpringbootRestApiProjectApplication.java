b8abc5a94de2ffbc40318bc03c18036c68c22eb2
Kalpadith <ikalpadith@gmail.com>
2024-02-01 19:10:27 +0530

    Initialized the spring project

---
+++ src/main/java/com/rest/JavaSpringbootRESTapiproject/JavaSpringbootRestApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rest.JavaSpringbootRESTapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootRestApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootRestApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/JavaSpringbootRESTapiproject/JavaSpringbootRestApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.JavaSpringbootRESTapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootRestApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
