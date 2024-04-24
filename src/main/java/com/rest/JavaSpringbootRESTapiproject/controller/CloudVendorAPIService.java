6026805a508c2c2580a704ccfc46206b6e3d9b5a
Kalpadith <ikalpadith@gmail.com>
2024-04-24 14:33:54 +0530

    created API controller to create vendor details

--- src/main/java/com/rest/JavaSpringbootRESTapiproject/controller/CloudVendorAPIService.java
+++ src/main/java/com/rest/JavaSpringbootRESTapiproject/controller/CloudVendorAPIService.java
@@ -4,15 +4,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.rest.JavaSpringbootRESTapiproject.model.CloudVendor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping ("/cloudvendor")
 public class CloudVendorAPIService {
 
+	CloudVendor cloudVendor;
+	
 	@GetMapping("{VendorId}")
 	public CloudVendor getCloudVendorDetails(String VendorId) {
 		return new CloudVendor("V1", "Ishan  Kalpadith", "Isuru niwasa,Mrungasyaya west,Middeniya.","0769733135");
-		
-		
+		//return cloudVendor;
 	}
+	
+	@PostMapping("/create")
+	public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor Created Successfully";
+	}
+	
 }
